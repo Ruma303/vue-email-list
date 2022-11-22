f123b5ee0c8ca3d2cd13a655fd278ee2df108974
Ruma <matteoh8@hotmail.it>
2022-11-22 15:34:58 +0100

    reset array ok

--- js/script.js
+++ js/script.js
@@ -1,21 +1,19 @@
 const app = new Vue ({
     el: '#app',
      data: {
-         email: '',
-         arrEmails:[],
+        arrEmails:[],
      }, methods: {
-          renderEmails(i) {
+          renderEmails() {
             for (let i=0; i<10; i++) {
+                this.arrEmails = []; //Svuoto l'array ad ogni click
                  //API richiesta di una mail
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                 .then(axiosEmail => { //Mi restituisce una mail
+                .then(axiosEmail => { //Mi restituisce una mail
                     console.log(axiosEmail);
-                    this.email = axiosEmail.data.response; //Inserisco la mail nella variabile
-                    this.arrEmails.push(axiosEmail.data.response); //Pusho la mail nell'array
-                 }); 
+                    this.arrEmails.push(axiosEmail.data.response); //Pusho ogni mail nell'array
+                }); 
             }; 
             console.log(this.arrEmails);
-            this.arrEmails = this.axiosEmail.data.response; //Reset array al clik non funziona
          },
      },
  });
